6a7bd7cbd6165a5e6d2cc7b6a2b3f40689c30d17
Limon4egtop <91752425+Limon4egtop@users.noreply.github.com>
2023-06-15 22:52:01 +0300

    какой-то ким

--- 24.py
+++ 24.py
@@ -366,18 +366,18 @@ print(max(len(x) for x in s))
 # print(max(len(x) for x in f))
 
 
-f = open("24.txt").read()
-
-maxLen = -1000
-startI = -1000
-stopI=0
-for i in range(len(f)-4):
-    if f[i+1] == "B" and f[i+4] == "D" and stopI < startI:
-        startI = i+1
-    if f[i + 1] == "B" and f[i + 4] == "D" and stopI > startI:
-        stopI = i+4
-        maxLen = max(maxLen, stopI-startI)
-print(maxLen)
+# f = open("24.txt").read()
+#
+# maxLen = -1000
+# startI = -1000
+# stopI=0
+# for i in range(len(f)-4):
+#     if f[i+1] == "B" and f[i+4] == "D" and stopI < startI:
+#         startI = i+1
+#     if f[i + 1] == "B" and f[i + 4] == "D" and stopI > startI:
+#         stopI = i+4
+#         maxLen = max(maxLen, stopI-startI)
+# print(maxLen)
 
 
 
